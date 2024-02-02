5ea81a4f393205b5438154748135ccec77265e4a
thanhngodev <thanhngodev@gmail.com>
2024-02-03 01:07:36 +0700

    comment done

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
-// import commentRoutes from './routes/comment.route.js';
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
-// app.use('/api/comment', commentRoutes);
+app.use('/api/comment', commentRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');

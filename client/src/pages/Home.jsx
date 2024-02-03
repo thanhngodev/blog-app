0d134aec29600ad06756127a1e951c60e5665618
thanhngodev <thanhngodev@gmail.com>
2024-02-03 13:11:43 +0700

    handle dash done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+  return <div>Home</div>;
+};
 
-export default Home
-
+export default Home;
